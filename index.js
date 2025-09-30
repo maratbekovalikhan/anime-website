53bb5c9b24314ddfa87946ad64930bb804b72562
maratbekovalikhan <maratbekovalikhan@gmail.com>
2025-09-30 13:02:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const header = document.getElementById('genres-header');
-const list = document.getElementById('genres-list');
-const arrow = document.getElementById('arrow');
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.getElementById('genres-header');
+  const list = document.getElementById('genres-list');
 
-header.addEventListener('click', () => {
-  list.classList.toggle('hidden');
-  header.classList.toggle('active');
+  header.addEventListener('click', () => {
+    list.classList.toggle('hidden');
+    header.classList.toggle('active');
+  });
 });
